722352f4d9dc541e39dbf8f5d24ba62fa891768e
wap4444 <wap4444@mail.ru>
2018-07-19 12:13:47 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -19,19 +19,20 @@ var app = {
     },
 
     receivedEvent: function(id) {
-document.addEventListener("offline", onOffline, false);
+	document.addEventListener("offline", onOffline, false);
+
 function onOffline() {
 $('#BtnEnt').hide();
 $('#inetOff').show();
 ref.close();
 }
 	    
-document.addEventListener("online", onOnline, false);
-function onOnline() {
-var ref = cordova.InAppBrowser.open('http://poruch.mirada.kz/fr7/?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
-$('#inetOff').hide();
-$('#BtnEnt').show();
-}
+//document.addEventListener("online", onOnline, false);
+//function onOnline() {
+//var ref = cordova.InAppBrowser.open('http://smart-pavlodar.kz/pavlodar/smart/?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+//$('#inetOff').hide();
+//$('#BtnEnt').show();
+//}
 
 if(localStorage.ipush){}
 else{
@@ -42,7 +43,7 @@ $('.loader1').fadeIn();
 
 
 $("#BtnEnt" ).click(function() {
-var ref = cordova.InAppBrowser.open('http://poruch.mirada.kz/fr7/', '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+var ref = cordova.InAppBrowser.open('http://smart-pavlodar.kz/pavlodar/smart/?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 });
         
 function didReceiveRemoteNotificationCallBack(jsonData) {}
@@ -65,7 +66,7 @@ window.plugins.OneSignal.getIds(function(ids) {
 ipush = ids.userId;
 $('.loader1').hide();
 localStorage.ipush=ipush;
-var ref = cordova.InAppBrowser.open('http://poruch.mirada.kz/fr7/', '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+var ref = cordova.InAppBrowser.open('http://smart-pavlodar.kz/pavlodar/smart/?push='+ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 $('.loader2').show();
 });
         
